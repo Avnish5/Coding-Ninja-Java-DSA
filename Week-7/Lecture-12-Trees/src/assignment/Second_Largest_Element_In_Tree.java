adc038a0d52ddd0b6acb35ed5271eb5cf4de2dae
Avnish Kumar <avnein4988@gmail.com>
2024-08-22 13:01:41 +0530

    Second_Largest_Element_In_Tree

--- Week-7/Lecture-12-Trees/src/assignment/Second_Largest_Element_In_Tree.java
+++ Week-7/Lecture-12-Trees/src/assignment/Second_Largest_Element_In_Tree.java
@@ -4,44 +4,46 @@ import lecture_12_trees.TreeNode;
 
 public class Second_Largest_Element_In_Tree {
 
-    static class Pair<T>{
-        T first;
-        T second;
-        Pair(T first,T second){
-            this.first = first;
-            this.second = second;
+    static class Result{
+        TreeNode<Integer> largest=null;
+        TreeNode<Integer> secondLargest=null;
+    }
+    public static TreeNode<Integer> findSecondLargestHelper(TreeNode<Integer> root)
+    {
+        Result result=new Result();
+        traverse(root,result);
+        if(result.largest!=null && result.secondLargest!=null && result.largest.data==result.secondLargest.data) return null;
+        return result.secondLargest;
+    }
+
+    public static void traverse(TreeNode<Integer> root,Result result)
+    {
+        if(root==null) return;
+
+        if(result.largest!=null && result.secondLargest!=null && result.largest.data==result.secondLargest.data  )
+        {
+            result.secondLargest=root;
+        }
+        else if(result.largest==null  || root.data>result.largest.data)
+        {
+            result.secondLargest=result.largest;
+            result.largest=root;
+        }
+        else if(result.secondLargest==null || root.data>result.secondLargest.data&&root.data<result.largest.data)
+        {
+            result.secondLargest=root;
+        }
+
+        for(TreeNode<Integer> child:root.children)
+        {
+            traverse(child, result);
         }
     }
     public static TreeNode<Integer> findSecondLargest(TreeNode<Integer> root){
 
+        if(root==null) return root;
+        return findSecondLargestHelper(root);
 
-        // Write your code here
 
-        return findSecondLargestT(root).second;
-    }
-    public static Pair<TreeNode<Integer>> findSecondLargestT(TreeNode<Integer> root){
-        Pair<TreeNode<Integer>> output;
-        if(root == null){
-            output = new Pair<TreeNode<Integer>>(null,null);
-            return output;
-        }
-        output = new Pair<TreeNode<Integer>>(root,null);
-        for(TreeNode<Integer> child : root.children){
-            Pair<TreeNode<Integer>> childPair = findSecondLargestT(child);
-            if(childPair.first.data > output.first.data){
-                if(childPair.second==null||childPair.second.data < output.first.data){
-                    output.second = output.first;
-                    output.first = childPair.first;
-                } else {
-                    output.first = childPair.first;
-                    output.second = childPair.second;
-                }
-            } else if(childPair.first.data.equals(output.first.data) && childPair.second != null){
-                output.second = childPair.second;
-            } else if(output.first.data != childPair.first.data && (output.second == null || childPair.first.data>output.second.data)){
-                output.second = childPair.first;
-            }
-        }
-        return output;
     }
 }
