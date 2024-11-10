1534073ec996e2fd2605b85b8760e86db359ad30
Avnish Kumar <avnein4988@gmail.com>
2024-11-10 21:04:37 +0530

    learn about union find algorithm and how we can improve it using path compression

---
+++ Week-13/Lecture-24-Graph-2/src/lecture_24_graph_2/Union_Find_Algo_By_Rank.java
@@ -0,0 +1,47 @@
+package lecture_24_graph_2;
+
+public class Union_Find_Algo_By_Rank {
+
+    int[] parent;
+    int[] rank;
+
+    Union_Find_Algo_By_Rank(int size){
+        parent=new int[size];
+        rank=new int[size];
+    }
+
+    public int find(int i)
+    {
+        if(parent[i]!=i)
+        {
+            parent[i]=find(parent[i]);
+        }
+
+        return parent[i];
+    }
+
+    public void unionByRank(int i,int j)
+    {
+        int irep=find(i);
+        int jrep=find(j);
+
+        if (irep == jrep)
+            return;
+
+        int irank=rank[irep];
+        int jrank=rank[jrep];
+
+        if(irank<jrank)
+        {
+            parent[irep]=jrep;
+        }
+        else if(jrank<irank)
+        {
+            parent[jrep]=irep;
+        }
+        else{
+            parent[irep]=jrep;
+            rank[jrep]++;
+        }
+    }
+}
